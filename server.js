63492b7c46cc3adb2a6c4c763c30a0408d4f0edb
salociN <clazzicx@web.de>
2024-05-26 15:21:10 +0200

    register added

--- server.js
+++ server.js
@@ -1,71 +1,19 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
-const { buildSchema } = require('graphql');
 const dotenv = require('dotenv');
-const User = require('./models/User');  // Importiere das Modell
+const authRoutes = require('./routes/auth');
 
 dotenv.config();
 
 const app = express();
 
-// MongoDB-Verbindung
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
+app.use(express.json());
 
-// GraphQL-Schema
-const schema = buildSchema(`
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-  }
-
-  type Query {
-    hello: String
-    getUser(id: ID!): User
-  }
-
-  type Mutation {
-    createUser(name: String!, email: String!): User
-    updateUser(id: ID!, name: String, email: String): User
-    deleteUser(id: ID!): String
-  }
-`);
-
-const root = {
-    hello: () => {
-        return 'Test123';
-    },
-    getUser: async ({ id }) => {
-        return await User.findById(id);
-    },
-    createUser: async ({ name, email }) => {
-        const user = new User({ name, email });
-        await user.save();
-        return user;
-    },
-    updateUser: async ({ id, name, email }) => {
-        return await User.findByIdAndUpdate(id, { name, email }, { new: true });
-    },
-    deleteUser: async ({ id }) => {
-        await User.findByIdAndDelete(id);
-        return 'User deleted';
-    }
-};
-
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/graphql`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
